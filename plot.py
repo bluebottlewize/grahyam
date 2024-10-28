09177d2cca215f9a91df5b011b28af388d389b50
bluebottlewize <udithkrishna.nair@research.iiit.ac.in>
2024-10-28 14:04:27 +0530

    added normalization

---
+++ plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+# Define the coordinates
+coords = open("coords", "r").readlines()
+
+coordinates = []
+
+for i in coords:
+    x = i.split()
+    x = (int(x[0]), int(x[1]))
+    coordinates.append(x)
+
+print(coordinates)
+
+# Split the coordinates into x and y values
+x, y = zip(*coordinates)
+
+# Create the scatter plot
+plt.scatter(x, y)
+
+plt.plot(x, y, color='orange', linestyle='-', marker='o')
+
+plt.xlim(min(x) - 10, max(x) + 10)
+plt.ylim(min(y) - 10, max(y) + 10)
+
+# plt.gca().invert_xaxis()
+plt.gca().invert_yaxis()
+
+# Add titles and labels
+plt.title('Scatter Plot of Coordinates')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+
+# Show grid
+plt.grid()
+
+# Display the plot
+plt.show()
+
